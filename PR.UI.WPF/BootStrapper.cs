00720b1d6fd13f8225f2f3bf9e02d2fd6e79b887
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-28 10:26:34 +0100

    -

---
+++ PR.UI.WPF/BootStrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Configuration;
+using PR.Persistence.Versioned;
+using StructureMap;
+using PR.ViewModel;
+using PR.Persistence;
+
+namespace PR.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                try
+                {
+                    var mainWindowViewModel = Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+
+                    var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                    var settings = configFile.AppSettings.Settings;
+                    var versioning = settings["Versioning"]?.Value;
+                    var reseeding = settings["Reseeding"]?.Value;
+
+                    if (versioning == "enabled")
+                    {
+                        // Den skal ikke wrappes, hvis det er en af dem, der repræsenterer et API
+                        if (mainWindowViewModel.UnitOfWorkFactory is not IUnitOfWorkFactoryVersioned)
+                        {
+                            // Wrap the UnitOfWorkFactory, so we get versioning and history
+                            mainWindowViewModel.UnitOfWorkFactory =
+                                new UnitOfWorkFactoryFacade(mainWindowViewModel.UnitOfWorkFactory);
+                        }
+                    }
+
+                    mainWindowViewModel.Initialize(
+                        versioning == "enabled",
+                        reseeding == "enabled");
+
+                    return mainWindowViewModel;
+                }
+                catch (ConfigurationErrorsException)
+                {
+                    System.Console.WriteLine("Error reading app settings");
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ PR.UI.WPF/MainWindowViewModelRegistry.cs
@@ -0,0 +1,19 @@
+﻿using StructureMap;
+
+namespace PR.UI.WPF
+{
+    public class MainWindowViewModelRegistry : Registry
+    {
+        public MainWindowViewModelRegistry()
+        {
+            Scan(_ =>
+            {
+                _.WithDefaultConventions();
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("Craft.Logging"));
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("Craft.UIElements"));
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR"));
+                _.LookForRegistries();
+            });
+        }
+    }
+}
